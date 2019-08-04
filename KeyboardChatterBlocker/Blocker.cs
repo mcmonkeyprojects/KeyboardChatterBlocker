8f2dc68436703734c539152bff9ba752cf54e3d4
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-08-04 16:40:13 -0700

    Initial commit: mostly empty app

---
+++ KeyboardChatterBlocker/Blocker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyboardChatterBlocker
+{
+    /// <summary>
+    /// Primary keyboard chatter blocker class.
+    /// </summary>
+    public class Blocker
+    {
+    }
+}
